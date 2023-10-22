ef642957b821167b04d9a8b5203d791f9573988e
AdamTaylor9 <tayloradamross@gmail.com>
2023-10-22 02:11:33 -0500

    Added X & O input functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,4 +5,24 @@ document.addEventListener("DOMContentLoaded", function() {
     squares.forEach(function (div) {
     div.classList.add("square");
     });
+
+    //adding x's and o's
+    var input = "X";
+    var gamestate = [];
+    board.addEventListener("click", function(event) {
+        if (event.target.classList.contains("square")) {
+            const selectedSquare = event.target;
+
+            selectedSquare.classList.add(input);
+            selectedSquare.textContent = input;
+
+            if (input== "X")
+                input = "O";
+            else
+                input = "X";
+        }
+    });
+
 });
+
+
