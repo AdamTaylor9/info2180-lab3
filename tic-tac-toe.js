faae966aad96c51f2ec28accac0ef274674d7844
AdamTaylor9 <tayloradamross@gmail.com>
2023-10-22 03:48:18 -0500

    Added check for winner and win status

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,34 +1,60 @@
+function winCon(gamestate){
+    const win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8],[0, 4, 8], [2, 4, 6]];
+
+    for (const condition of win) {
+        const [a, b, c] = condition;
+        if (gamestate[a] && gamestate[a] == gamestate[b] && gamestate[b] == gamestate[c] && gamestate[c]) {
+          return [true, gamestate[a]];
+        }
+      }
+
+    return [false];
+
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const board = document.getElementById("board");
     const squares = board.querySelectorAll("div");      
 
     squares.forEach(function (div) {
-    div.classList.add("square");
+        div.classList.add("square");
     });
 
     //adding x's and o's
     var input = "X";
-    var gamestate = [];
-    board.addEventListener("click", function(event) {
-        if (event.target.classList.contains("square")) {
-            const selectedSquare = event.target;
+    var gamestate = ["","","","","","","","",""];
 
-            selectedSquare.classList.add(input);
-            selectedSquare.textContent = input;
-
+    squares.forEach((cell, index) => {
+        cell.addEventListener('click', () => {
+            cell.classList.add(input);
+            cell.textContent = input;
+            gamestate[index]=input;
             if (input== "X")
                 input = "O";
             else
                 input = "X";
-        }
-    });
+            // console.log(gamestate);
+            winstat = winCon(gamestate);
+            if (winstat[0]){
+                gamestate = ["","","","","","","","",""];
+                const status=document.getElementById("status");
+                status.classList.add("you-won");
+                var winMessage;
+                if (winstat[1]=="X")
+                    winMessage = 'Congratulations! X is the Winner!';
+                else
+                    winMessage = 'Congratulations! O is the Winner!';
+                status.textContent = winMessage;
+                
+            }
+        });
+      });
 
     //adding hover
     board.addEventListener("mouseover", function(event) {
         if (event.target.classList.contains("square")) {
             const square = event.target;
             square.classList.add("hover");
-            console.log("On");
         }
     });
 
